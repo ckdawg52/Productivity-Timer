42b820683ca3337c4bb19658f75f09d6e9e99768
ckdawg52 <ckdawg5250@gmail.com>
2026-04-28 18:05:20 +0000

    Added input options and some conversions

--- src/main.py
+++ src/main.py
@@ -1,31 +1,57 @@
 import time
 import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--duration", type=float)
+parser.add_argument("-i", "--interval", type=float)
+args = parser.parse_args()
+
+duration = args.duration
+interval = args.interval
+
+if duration is None:
+    duration = float(input("How long would you like your focus session? "))
+if interval is None:
+    interval = float(input("How long would you like the intervals to be? "))
+
 def countdown():
     print("Starting timer...")
     time.sleep(1.2)
-    print("3")
+    print("3\r", end = "")
     time.sleep(1)
-    print("2")
+    print("2\r", end = "")
     time.sleep(1)
-    print("1")
+    print("1\r", end = "")
     time.sleep(1)
 
 def timer(count):
     start_time = time.time()
-    while time.time() - start_time < count:
-        time_left = (count - (time.time() - start_time)) // 1
-        print(f"Time left: {time_left} seconds\r", end = "")
-        time.sleep(1)
+    while time.time() - start_time < count + 1:
+        time_left = ((count - (time.time() - start_time)) // 1) + 1
+        if time_left > 60:
+            minutes = time_left // 60
+            seconds = time_left % 60
+            print(f"Time left: {minutes} minutes and {seconds} seconds\r", end = "")
+            time.sleep(1)
+        else:
+            print(f"Time left: {time_left} seconds                     \r", end = "")
+            time.sleep(1)
+
+def minutes_to_seconds(minutes):
+    return minutes * 60
+
 
 def timer_loop(duration, interval):
-    cycles = duration // interval
+    duration = minutes_to_seconds(duration)
+    interval = minutes_to_seconds(interval)
+    cycles = int(duration) // int(interval) 
     count = 1
     countdown()
     for i in range(cycles):
         if count > 1:
             print(f"\nTimer {count}")
         count += 1
-        timer(interval)
+        timer(int(interval))
 
-timer_loop(60, 10)
+
+timer_loop(duration, interval)
