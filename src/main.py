8d19303aee0ea640076870126e56a2ebbd37b9c7
ckdawg52 <ckdawg5250@gmail.com>
2026-04-28 16:21:02 +0000

    Made timer static so it doesn't print a new line every second.

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import time
+import argparse
+
+def countdown():
+    print("Starting timer...")
+    time.sleep(1.2)
+    print("3")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("1")
+    time.sleep(1)
+
+def timer(count):
+    start_time = time.time()
+    while time.time() - start_time < count:
+        time_left = (count - (time.time() - start_time)) // 1
+        print(f"Time left: {time_left} seconds\r", end = "")
+        time.sleep(1)
+
+def timer_loop(duration, interval):
+    cycles = duration // interval
+    count = 1
+    countdown()
+    for i in range(cycles):
+        if count > 1:
+            print(f"\nTimer {count}")
+        count += 1
+        timer(interval)
+
+timer_loop(60, 10)
