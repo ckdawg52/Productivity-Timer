4c5a9ce348f80d9ef2ad5bf0808f9215e23b4de5
ckdawg52 <ckdawg5250@gmail.com>
2026-04-29 16:41:20 +0000

    Sanity save

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
+
 import argparse
 import os
 import time
-
 import alive_progress
 from terminaltexteffects.effects.effect_slide import Slide
 from terminaltexteffects.effects.effect_smoke import Smoke
@@ -19,7 +19,6 @@ interval = args.interval
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
-
 def wave_animated_text(text: str) -> str:
     effect = Waves(text)
     effect.effect_config.final_gradient_frames = 1
@@ -27,14 +26,12 @@ def wave_animated_text(text: str) -> str:
         for frame in effect:
             terminal.print(frame)
 
-
 def smoke_animated_text(text: str) -> str:
     effect = Smoke(text)
     effect.effect_config.final_gradient_frames = 1
     with effect.terminal_output(end_symbol=" ") as terminal:
         for frame in effect:
             terminal.print(frame)
-
 
 def slide_animated_prompt(prompt_text: str) -> str:
     effect = Slide(prompt_text)
@@ -50,7 +47,6 @@ def slide_animated_text(prompt_text: str) -> str:
     with effect.terminal_output(end_symbol=" ") as terminal:
         for frame in effect:
             terminal.print(frame)
- 
 
 
 if duration is None and interval is None:
@@ -60,6 +56,7 @@ if duration is None and interval is None:
   \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|     | || | | |
    \ V  V / | |___| |__| |__| |_| | |  | | |___    | || |_| |
     \_/\_/  |_____|_____\____\___/|_|  |_|_____|   |_| \___/
+
 """
     text2 = r"""
   _____ ___   ____ _   _ ____    _____ ___ __  __ _____ ____
@@ -72,7 +69,7 @@ if duration is None and interval is None:
 """
     clear()
     wave_animated_text(text1 + text2)
-    
+   
 
 if duration is None:
     duration = float(
@@ -85,14 +82,13 @@ if interval is None:
         )
     )
 
-
 def countdown():
     clear()
     slide_animated_text(r"""
   ____ _____  _    ____ _____ ___ _   _  ____   _____ ___ __  __ _____ ____              
- / ___|_   _|/ \  |  _ \_   _|_ _| \ | |/ ___| |_   _|_ _|  \/  | ____|  _ \             
+ / ___|_   _|/ \  |  _ \_   _|_ _| \ | |/ ___| |_   _|_ _|  \/  | ____|  _ \            
  \___ \ | | / _ \ | |_) || |  | ||  \| | |  _    | |  | || |\/| |  _| | |_) |            
-  ___) || |/ ___ \|  _ < | |  | || |\  | |_| |   | |  | || |  | | |___|  _ <   _   _   _ 
+  ___) || |/ ___ \|  _ < | |  | || |\  | |_| |   | |  | || |  | | |___|  _ <   _   _   _
  |____/ |_/_/   \_\_| \_\|_| |___|_| \_|\____|   |_| |___|_|  |_|_____|_| \_\ (_) (_) (_)
                                                                                          
                                                                                          
@@ -100,9 +96,9 @@ def countdown():
     time.sleep(1.2)
     clear()
     slide_animated_text(r"""  
-  _____ 
- |___ / 
-   |_ \ 
+  _____
+ |___ /
+   |_ \
   ___) |
  |____/
 
@@ -111,37 +107,36 @@ def countdown():
     clear()
     slide_animated_text(r"""
   ____  
- |___ \ 
+ |___ \
    __) |
-  / __/ 
+  / __/
  |_____|
-        
+
     """)
     time.sleep(1)
     clear()
     slide_animated_text(r"""
-  _ 
+  _
  / |
  | |
  | |
  |_|
-    
+
     """)
     time.sleep(1)
     clear()
     slide_animated_text(r"""
-  ____ _____  _    ____ _____ 
+  ____ _____  _    ____ _____
  / ___|_   _|/ \  |  _ \_   _|
  \___ \ | | / _ \ | |_) || |  
   ___) || |/ ___ \|  _ < | |  
  |____/ |_/_/   \_\_| \_\|_|  
-                              
+                            
     """)
-
 
 def timer(count):
     start_time = time.time()
-    with alive_progress.alive_bar(count + 1, title="Timer active. Stay focused!", spinner=None) as bar:
+    with alive_progress.alive_bar(count + 1, title="Timer active. Stay focused!", spinner=None, receipt=False, monitor=False, elapsed=False, stats=False) as bar:
         while time.time() - start_time < count + 1:
             time_left = ((count - (time.time() - start_time)) // 1) + 1
             if time_left > 60:
@@ -155,10 +150,8 @@ def timer(count):
                 time.sleep(1)
                 bar()
 
-
 def minutes_to_seconds(minutes):
     return minutes * 60
-
 
 def timer_loop(duration, interval):
     duration = minutes_to_seconds(duration)
@@ -185,8 +178,6 @@ def timer_loop(duration, interval):
 """
     countdown()
     for i in range(cycles):
-        #       if count > 1:
-        #          print(f"\nTimer {count}")
         count += 1
         timer(int(interval))
         clear()
@@ -196,6 +187,5 @@ def timer_loop(duration, interval):
         else:
             print("\a")
             smoke_animated_text(finish)
-
 
 timer_loop(duration, interval)
