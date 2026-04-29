374f33d7cf42e2a740bc791b9747debc017671db
ckdawg52 <ckdawg5250@gmail.com>
2026-04-28 20:52:33 -0600

    turned strings into raw strings to prevent error from showing up.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-import time
-import os
 import argparse
+import os
+import time
+
 import alive_progress
-from terminaltexteffects.effects.effect_waves import Waves
 from terminaltexteffects.effects.effect_slide import Slide
 from terminaltexteffects.effects.effect_smoke import Smoke
+from terminaltexteffects.effects.effect_waves import Waves
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--duration", type=float)
@@ -14,23 +15,26 @@ args = parser.parse_args()
 duration = args.duration
 interval = args.interval
 
+
 def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 def wave_animated_text(text: str) -> str:
     effect = Waves(text)
     effect.effect_config.final_gradient_frames = 1
-    with effect.terminal_output(end_symbol = " ") as terminal:
+    with effect.terminal_output(end_symbol=" ") as terminal:
         for frame in effect:
             terminal.print(frame)
+
 
 def smoke_animated_text(text: str) -> str:
     effect = Smoke(text)
     effect.effect_config.final_gradient_frames = 1
-    with effect.terminal_output(end_symbol = " ") as terminal:
+    with effect.terminal_output(end_symbol=" ") as terminal:
         for frame in effect:
             terminal.print(frame)
+
 
 def slide_animated_prompt(prompt_text: str) -> str:
     effect = Slide(prompt_text)
@@ -40,20 +44,21 @@ def slide_animated_prompt(prompt_text: str) -> str:
             terminal.print(frame)
     return input()
 
+
 if duration is None and interval is None:
-    text1 = """
- __        _______ _     ____ ___  __  __ _____   _____ ___  
- \ \      / / ____| |   / ___/ _ \|  \/  | ____| |_   _/ _ \ 
+    text1 = r"""
+ __        _______ _     ____ ___  __  __ _____   _____ ___
+ \ \      / / ____| |   / ___/ _ \|  \/  | ____| |_   _/ _ \
   \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|     | || | | |
-   \ V  V / | |___| |__| |__| |_| | |  | | |___    | || |_| | 
-    \_/\_/  |_____|_____\____\___/|_|  |_|_____|   |_| \___/ 
+   \ V  V / | |___| |__| |__| |_| | |  | | |___    | || |_| |
+    \_/\_/  |_____|_____\____\___/|_|  |_|_____|   |_| \___/
 """
-    text2 = """
-  _____ ___   ____ _   _ ____    _____ ___ __  __ _____ ____  
- |  ___/ _ \ / ___| | | / ___|  |_   _|_ _|  \/  | ____|  _ \ 
+    text2 = r"""
+  _____ ___   ____ _   _ ____    _____ ___ __  __ _____ ____
+ |  ___/ _ \ / ___| | | / ___|  |_   _|_ _|  \/  | ____|  _ \
  | |_ | | | | |   | | | \___ \    | |  | || |\/| |  _| | |_) |
- |  _|| |_| | |___| |_| |___) |   | |  | || |  | | |___|  _ < 
- |_|   \___/ \____|\___/|____/    |_| |___|_|  |_|_____|_| \_\\
+ |  _|| |_| | |___| |_| |___) |   | |  | || |  | | |___|  _ <
+ |_|   \___/ \____|\___/|____/    |_| |___|_|  |_|_____|_| \_\
 
 
 """
@@ -62,36 +67,44 @@ if duration is None and interval is None:
 
 
 if duration is None:
-    duration = float(slide_animated_prompt("How long would you like your focus session in minutes? "))
+    duration = float(
+        slide_animated_prompt("How long would you like your focus session in minutes? ")
+    )
 if interval is None:
-    interval = float(slide_animated_prompt("How long would you like the intervals to be in minutes? "))
+    interval = float(
+        slide_animated_prompt(
+            "How long would you like the intervals to be in minutes? "
+        )
+    )
+
 
 def countdown():
     print("Starting timer...")
     time.sleep(1.2)
-    print("3\r", end = "")
+    print("3\r", end="")
     time.sleep(1)
-    print("2\r", end = "")
+    print("2\r", end="")
     time.sleep(1)
-    print("1\r", end = "")
+    print("1\r", end="")
     time.sleep(1)
+
 
 def timer(count):
     start_time = time.time()
     with alive_progress.alive_bar(count + 1) as bar:
-        
         while time.time() - start_time < count + 1:
             time_left = ((count - (time.time() - start_time)) // 1) + 1
             if time_left > 60:
                 minutes = time_left // 60
                 seconds = time_left % 60
-                #print(f"Time left: {minutes} minutes and {seconds} seconds\r", end = "")
+                # print(f"Time left: {minutes} minutes and {seconds} seconds\r", end = "")
                 time.sleep(1)
                 bar()
             else:
-                #print(f"Time left: {time_left} seconds                     \r", end = "")
+                # print(f"Time left: {time_left} seconds                     \r", end = "")
                 time.sleep(1)
                 bar()
+
 
 def minutes_to_seconds(minutes):
     return minutes * 60
@@ -100,28 +113,29 @@ def minutes_to_seconds(minutes):
 def timer_loop(duration, interval):
     duration = minutes_to_seconds(duration)
     interval = minutes_to_seconds(interval)
-    cycles = int(duration) // int(interval) 
+    cycles = int(duration) // int(interval)
     count = 1
-    focus = """
-  _____ ___   ____ _   _ ____  
- |  ___/ _ \ / ___| | | / ___|                                                                                                                                         
- | |_ | | | | |   | | | \___ \                                                                                                                                          
- |  _|| |_| | |___| |_| |___) |                                                                                                                                              
- |_|   \___/ \____|\___/|____/                                                                                                                                                 
-                               
+    focus = r"""
+  _____ ___   ____ _   _ ____
+ |  ___/ _ \ / ___| | | / ___|
+ | |_ | | | | |   | | | \___ \
+ |  _|| |_| | |___| |_| |___) |
+ |_|   \___/ \____|\___/|____/
+
 \n
 """
-    finish = """
-  _____ ___ _   _ ___ ____  _   _ _____ ____  
- |  ___|_ _| \ | |_ _/ ___|| | | | ____|  _ \                                                                                                                    
- | |_   | ||  \| || |\___ \| |_| |  _| | | | |                                                                                                                            
+    finish = r"""
+  _____ ___ _   _ ___ ____  _   _ _____ ____
+ |  ___|_ _| \ | |_ _/ ___|| | | | ____|  _ \
+ | |_   | ||  \| || |\___ \| |_| |  _| | | | |
  |  _|  | || |\  || | ___) |  _  | |___| |_| |
- |_|   |___|_| \_|___|____/|_| |_|_____|____/ \n                                                                                                                                                                      
+ |_|   |___|_| \_|___|____/|_| |_|_____|____/
+
 """
     countdown()
     for i in range(cycles):
-#       if count > 1:
-#          print(f"\nTimer {count}")
+        #       if count > 1:
+        #          print(f"\nTimer {count}")
         count += 1
         timer(int(interval))
         clear()
@@ -131,7 +145,6 @@ def timer_loop(duration, interval):
         else:
             print("\a")
             smoke_animated_text(finish)
-
 
 
 timer_loop(duration, interval)
